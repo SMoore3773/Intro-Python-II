3067bdbda544d7f87a9e64cdc699774d13731d90
SMoore3773 <Smooresculpture@gmail.com>
2020-07-15 21:17:03 -0400

    initial commit

--- src/player.py
+++ src/player.py
@@ -1,2 +1,10 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+from room import Room
+
+class Player:
+    def __init__(self, cur_room):
+        self.cur_room = cur_room
+        # self.room_loc = room_loc
+        # self.item_inv = item_inv
+        # self.alive = alive
--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+    def __init__(self, room_name, room_description):
+        self.room_name = room_name
+        self.room_description = room_description
+        # self.room_conents = room_conents
+        # self.room_items = room_items
+        # self.room_secret = room_secret
+        # self.room_doors = room_doors
